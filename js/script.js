2cf7202b5c5a1f263d885204a9956d11e6e4dacd
sharifulalam-dev <sharifulalam.tex@gmail.com>
2024-09-19 04:43:30 +0600

    home page update

--- js/script.js
+++ js/script.js
@@ -3,5 +3,12 @@ const phoneNumber = document.querySelector(".phone-number");
 const pinNumber = document.querySelector(".pin-number");
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(phoneNumber.value);
+  let userNumber = phoneNumber.value;
+  let userPass = pinNumber.value;
+
+  if (userNumber === "017" && userPass === "123") {
+    window.location.href = "/home.html";
+  } else {
+    alert("Enter Correct Phone Number and Password");
+  }
 });
